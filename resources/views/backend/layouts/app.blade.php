68ad730e78b8be486b33a376a2166fec4220a6f8
Parthadeb Mondal <Parthadev76@gmail.com>
2024-03-07 13:21:38 +0600

    backend sidebar

---
+++ resources/views/backend/layouts/app.blade.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <title>Admin Panel</title>
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('backend/images/favicon.png') }}">
+    <!-- Pignose Calender -->
+    <link href="{{ asset('backend/plugins/pg-calendar/css/pignose.calendar.min.css') }}" rel="stylesheet">
+    <!-- Chartist -->
+    <link rel="stylesheet" href="{{asset('backend/plugins/chartist/css/chartist.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('backend/plugins/chartist-plugin-tooltips/css/chartist-plugin-tooltip.css') }}">
+    <!-- Custom Stylesheet -->
+    <link href="{{ asset('backend/css/style.css') }}" rel="stylesheet">
+
+</head>
+
+<body>
+
+<!--*******************
+    Preloader start
+********************-->
+<div id="preloader">
+    <div class="loader">
+        <svg class="circular" viewBox="25 25 50 50">
+            <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="3" stroke-miterlimit="10" />
+        </svg>
+    </div>
+</div>
+<!--*******************
+    Preloader end
+********************-->
+
+
+<!--**********************************
+    Main wrapper start
+***********************************-->
+<div id="main-wrapper">
+
+    <!--**********************************
+        Nav header start
+    ***********************************-->
+
+    <!--**********************************
+        Nav header end
+    ***********************************-->
+
+    <!--**********************************
+        Header start
+    ***********************************-->
+        @include('backend.common.header')
+    <!--**********************************
+        Header end ti-comment-alt
+    ***********************************-->
+
+    <!--**********************************
+        Sidebar start
+    ***********************************-->
+        @include('backend.common.sidebar')
+    <!--**********************************
+        Sidebar end
+    ***********************************-->
+
+    <!--**********************************
+        Content body start
+    ***********************************-->
+    <div class="content-body">
+
+        <div class="container-fluid mt-3">
+            <div class="row">
+                <div class="col-lg-3 col-sm-6">
+                    <div class="card gradient-1">
+                        <div class="card-body">
+                            <h3 class="card-title text-white">Products Sold</h3>
+                            <div class="d-inline-block">
+                                <h2 class="text-white">4565</h2>
+                                <p class="text-white mb-0">Jan - March 2019</p>
+                            </div>
+                            <span class="float-right display-5 opacity-5"><i class="fa fa-shopping-cart"></i></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="card gradient-2">
+                        <div class="card-body">
+                            <h3 class="card-title text-white">Net Profit</h3>
+                            <div class="d-inline-block">
+                                <h2 class="text-white">$ 8541</h2>
+                                <p class="text-white mb-0">Jan - March 2019</p>
+                            </div>
+                            <span class="float-right display-5 opacity-5"><i class="fa fa-money"></i></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="card gradient-3">
+                        <div class="card-body">
+                            <h3 class="card-title text-white">New Customers</h3>
+                            <div class="d-inline-block">
+                                <h2 class="text-white">4565</h2>
+                                <p class="text-white mb-0">Jan - March 2019</p>
+                            </div>
+                            <span class="float-right display-5 opacity-5"><i class="fa fa-users"></i></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="card gradient-4">
+                        <div class="card-body">
+                            <h3 class="card-title text-white">Customer Satisfaction</h3>
+                            <div class="d-inline-block">
+                                <h2 class="text-white">99%</h2>
+                                <p class="text-white mb-0">Jan - March 2019</p>
+                            </div>
+                            <span class="float-right display-5 opacity-5"><i class="fa fa-heart"></i></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+        <!-- #/ container -->
+    </div>
+
+
+    <!--**********************************
+        Footer start
+    ***********************************-->
+    <div class="footer">
+        <div class="copyright">
+            <p>Copyright &copy; Designed & Developed by <a href="https://szamantech.com">SZamanTech</a>
+                {{date('Y')}}</p>
+        </div>
+    </div>
+    <!--**********************************
+        Footer end
+    ***********************************-->
+</div>
+<!--**********************************
+    Main wrapper end
+***********************************-->
+
+<!--**********************************
+    Scripts
+***********************************-->
+<script src="{{ asset('backend/plugins/common/common.min.js') }}"></script>
+<script src="{{ asset('backend/js/custom.min.js') }}"></script>
+<script src="{{ asset('backend/js/settings.js') }}"></script>
+<script src="{{ asset('backend/js/gleek.js') }}"></script>
+<script src="{{ asset('backend/js/styleSwitcher.js') }}"></script>
+
+<!-- Chartjs -->
+<script src="{{ asset('backend/plugins/chart.js/Chart.bundle.min.js') }}"></script>
+<!-- Circle progress -->
+<script src="{{ asset('backend/plugins/circle-progress/circle-progress.min.js') }}"></script>
+<!-- Datamap -->
+<script src="{{ asset('backend/plugins/d3v3/index.js') }}"></script>
+<script src="{{ asset('backend/plugins/topojson/topojson.min.js') }}"></script>
+<script src="{{ asset('backend/plugins/datamaps/datamaps.world.min.js') }}"></script>
+<!-- Morrisjs -->
+<script src="{{ asset('backend/plugins/raphael/raphael.min.js') }}"></script>
+<script src="{{ asset('backend/plugins/morris/morris.min.js') }}"></script>
+<!-- Pignose Calender -->
+<script src="{{ asset('backend/plugins/moment/moment.min.js') }}"></script>
+<script src="{{ asset('backend/plugins/pg-calendar/js/pignose.calendar.min.js') }}"></script>
+<!-- ChartistJS -->
+<script src="{{ asset('backend/plugins/chartist/js/chartist.min.js') }}"></script>
+<script src="{{ asset('backend/plugins/chartist-plugin-tooltips/js/chartist-plugin-tooltip.min.js') }}"></script>
+
+
+
+<script src="{{ asset('backend/js/dashboard/dashboard-1.js') }}"></script>
+
+</body>
+
+</html>
