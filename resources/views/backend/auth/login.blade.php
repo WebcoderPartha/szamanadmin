cdf7a3a3b00440394a35f8fe45e3ae08a17857f1
Parthadeb Mondal <Parthadev76@gmail.com>
2024-03-07 14:05:49 +0600

    Login page

--- resources/views/backend/auth/login.blade.php
+++ resources/views/backend/auth/login.blade.php
@@ -1,4 +1,5 @@
 @extends('backend.layouts.auth')
+@section('title', 'Login Page')
 @section('content')
     <div class="login-form-bg h-100">
         <div class="container h-100">
@@ -7,18 +8,18 @@
                     <div class="form-input-content">
                         <div class="card login-form mb-0">
                             <div class="card-body pt-5">
-                                <a class="text-center" href="index.html"> <h4>Rosella</h4></a>
-
-                                <form class="mt-5 mb-5 login-input">
+                                <h4 class="text-center">SZamanTech</h4>
+                                <form class="mt-5 mb-5 login-input" method="POST">
+                                    @csrf
                                     <div class="form-group">
-                                        <input type="email" class="form-control" placeholder="Email">
+                                        <input type="email" name="email" value="{{ old('email') }}" class="form-control" placeholder="Email">
                                     </div>
                                     <div class="form-group">
-                                        <input type="password" class="form-control" placeholder="Password">
+                                        <input type="password" name="password" value="{{ old('password') }}" class="form-control" placeholder="Password">
                                     </div>
-                                    <button class="btn login-form__btn submit w-100">Sign In</button>
+                                    <button type="submit" class="btn login-form__btn submit w-100">Sign In</button>
                                 </form>
-                                <p class="mt-5 login-form__footer">Dont have account? <a href="page-register.html" class="text-primary">Sign Up</a> now</p>
+
                             </div>
                         </div>
                     </div>
