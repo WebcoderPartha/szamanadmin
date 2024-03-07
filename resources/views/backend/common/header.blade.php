77a303a44603c16342f05f7329ff3c356a3a834b
Parthadeb Mondal <Parthadev76@gmail.com>
2024-03-07 13:27:57 +0600

    dsf

--- resources/views/backend/common/header.blade.php
+++ resources/views/backend/common/header.blade.php
@@ -145,23 +145,11 @@
                         </div>
                     </div>
                 </li>
-                <li class="icons dropdown d-none d-md-flex">
-                    <a href="javascript:void(0)" class="log-user"  data-toggle="dropdown">
-                        <span>English</span>  <i class="fa fa-angle-down f-s-14" aria-hidden="true"></i>
-                    </a>
-                    <div class="drop-down dropdown-language animated fadeIn  dropdown-menu">
-                        <div class="dropdown-content-body">
-                            <ul>
-                                <li><a href="javascript:void()">English</a></li>
-                                <li><a href="javascript:void()">Dutch</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </li>
+
                 <li class="icons dropdown">
                     <div class="user-img c-pointer position-relative"   data-toggle="dropdown">
                         <span class="activity active"></span>
-                        <img src="images/user/1.png" height="40" width="40" alt="">
+                        <img src="{{ asset('backend/images/user/1.png') }}" height="40" width="40" alt="">
                     </div>
                     <div class="drop-down dropdown-profile animated fadeIn dropdown-menu">
                         <div class="dropdown-content-body">
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('backend.layouts.app');
+    return view('backend.dashboard');
 });
 
 //Auth::routes();
